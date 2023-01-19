7d4bc98611568aa323160113226ae2e0e6cb4554
Kuvshinov Vlad <kuvshinov_v@mail.ru>
2023-01-19 20:25:52 +0300

    10 домашка - исправил кнопки соцсетей. теперь значки перекрашиваются в белый цвет а вон в красный + исправил размеры footerblock_1 и 2 и три, теперь они не выходят за границы допустимого

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 'use strict';
 
-
-// let menuActive = document.querySelector("div.popup_menu_active");
-// let burger = document.getElementsByClassName("div.nav2pict3");
-
-function toggleMenu()
-{
-    menuActive.classList.toggle("active");
-}
-
-// burger.addEventListener("click", toggleMenu() {});
-
-
-
-
-//   onclick="return toggleMenu(this)
-//Console.log("Кнопка работает!!!")
-
-
+//   Открываем менюшку из бургера
 let menuActive = document.querySelector("div.popup_menu_hidden");
 let burger = document.querySelector('.nav2pict3');
 burger.onclick = function() {menuActive.classList.toggle("active")};
+//-------------------------------------------------
+
+
